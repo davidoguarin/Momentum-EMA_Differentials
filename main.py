b0c96bc865cf3d0c6fdc44e49e4099ff72950338
davidoguarin <60156438+davidoguarin@users.noreply.github.com>
2025-09-01 19:35:01 -0500

    Update main.py

--- main.py
+++ main.py
@@ -17,7 +17,7 @@ from portfolio_simulation import run_momentum_portfolio_simulation
 from config import config
 
 # Configuration flags
-EXTRACT_DATA = False  # Set to True to fetch new data from API
+EXTRACT_DATA = True  # Set to True to fetch new data from API
 RUN_EMA_ANALYSIS = True  # Set to True to run EMA analysis
 RUN_PORTFOLIO_SIMULATION = True  # Set to True to run portfolio simulation
 DISPLAY_PLOTS = False  # Set to True to display plots, False to just save them
